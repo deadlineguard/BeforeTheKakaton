85fa21f0b768523ce845935ee8758969149a9800
Andrew <andreyy0strijov@gmail.com>
2021-10-12 22:50:17 +0500

    тут есть $ и ф-я

--- app/src/main/java/com/example/kotlin_lessons/MainActivity.kt
+++ app/src/main/java/com/example/kotlin_lessons/MainActivity.kt
@@ -1,47 +1,31 @@
 package com.example.kotlin_lessons
 
-import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
-import androidx.constraintlayout.widget.ConstraintLayout
 
 // https://www.youtube.com/playlist?list=PLmjT2NFTgg1clSDgx1YYOuVyZuCXVjfuR
 
 class MainActivity : AppCompatActivity() {
-    private var number: Int = 42
-    private var text1: TextView? = null // '?' после типа говорит о том, что мы насильно разрешаем
-                                        // переменной text1 иметь значение null
-    private var start: Boolean = false
-    private var counter: Int = 0
-    private var cLayout: ConstraintLayout? = null
+    private var number: Int = 6
+    private var number2: Int = 4
+    private var text1: String = "pepega has $number pepes" // $ перед переменной - впихивыает её в String
+    private var text2: String = "but pupuga has ${getNumber()} pepes" // с ф-ей тоже самое работает если заключить её в {}
+    private var telText: TextView? = null // '?' после типа говорит о том, что мы насильно разрешаем
+                                          // переменной text1 иметь значение null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        text1 = findViewById(R.id.text1)
-        text1?.setText(number.toString()) // знак '?' означает, что команда запустится только если
-                                          // text1 != null - а у нас != null т.к. мы присвоили знач-е выше
-        cLayout = findViewById(R.id.cLayout)
-        Thread{
-            start = true
-            while (start){
-                Thread.sleep(1000)
-
-                // изменять эл-ты UI со второстепенного потока нельзя,
-                // но если использовать runOnUiThread, то можно :)
-                runOnUiThread{
-                    if (counter == 5)
-                        cLayout?.setBackgroundColor(Color.BLUE)
-                    text1?.setText(counter.toString())
-                    counter++
-                }
-            }
-        }.start()
+        telText = findViewById(R.id.text1)
+        telText?.setText(text2)
     }
 
-    override fun onDestroy() {
-        super.onDestroy()
+    // функция, которая возвращает тип данных Int
+    // если после : ничего не указать, то ф-я не будет ничего возвращать (void)
+    private fun getNumber(): Int{
+        return number + number2
     }
+
 }
