6f9c13cd117c9decd5a5bc46754b809555d94def
Andrew <andreyy0strijov@gmail.com>
2021-10-13 08:08:25 +0500

    'примитивный парсинг' String

--- app/src/main/java/com/example/kotlin_lessons/MainActivity.kt
+++ app/src/main/java/com/example/kotlin_lessons/MainActivity.kt
@@ -7,25 +7,26 @@ import android.widget.TextView
 // https://www.youtube.com/playlist?list=PLmjT2NFTgg1clSDgx1YYOuVyZuCXVjfuR
 
 class MainActivity : AppCompatActivity() {
-    private var number: Int = 6
-    private var number2: Int = 4
-    private var text1: String = "pepega has $number pepes" // $ перед переменной - впихивыает её в String
-    private var text2: String = "but pupuga has ${getNumber()} pepes" // с ф-ей тоже самое работает если заключить её в {}
-    private var telText: TextView? = null // '?' после типа говорит о том, что мы насильно разрешаем
-                                          // переменной text1 иметь значение null
+    private var text: String = "В <магазине> осталось 123 яблока а может и <больше кто знает>"
+    private var telText: TextView? = null
+    private var telText2: TextView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
         telText = findViewById(R.id.text1)
-        telText?.setText(text2)
-    }
+        telText2 = findViewById(R.id.text3)
 
-    // функция, которая возвращает тип данных Int
-    // если после : ничего не указать, то ф-я не будет ничего возвращать (void)
-    private fun getNumber(): Int{
-        return number + number2
+        // "примитивный парсинг"
+        var subText: String = text.substringAfter('<') // находим первое совпадение с '<' и всё что дальше него - пихаем в subText
+        var subText2: String = subText.substringBefore('>') // неходим первое совпадение с '>' и всё что перед ним - пихаем в subText2
+        telText?.setText(subText2)
+
+        // а вот так можно вторую фразу захватить
+        var subText3: String = text.substringAfter('>')
+        var subText4: String = subText3.substringAfter('<')
+        var subText5: String = subText4.substringBefore('>')
+        telText2?.setText(subText5)
     }
 
 }
